5fce316ffda06f9b26c8b785f4b11f956e5c5df2
Vitse Maxime <maxime.vitse@decathlon.com>
2022-11-27 18:22:04 +0100

    [minipaint] TP3 - sélection et déplacement multiples

--- minipaint/src/main/java/drawing/handler/SelectionHandler.java
+++ minipaint/src/main/java/drawing/handler/SelectionHandler.java
@@ -14,7 +14,7 @@ public class SelectionHandler implements EventHandler<MouseEvent> {
 
     public SelectionHandler(DrawingPane drawingPane) {
         this.drawingPane = drawingPane;
-        listSelectedShape = new ArrayList<>();
+        this.listSelectedShape = new ArrayList<>();
         drawingPane.addEventHandler(MouseEvent.MOUSE_PRESSED, this);
     }
 
@@ -24,15 +24,16 @@ public class SelectionHandler implements EventHandler<MouseEvent> {
         if (event.getEventType().equals(MouseEvent.MOUSE_PRESSED)) {
             for (IShape shape : drawingPane) {
                 if (shape.isOn(event.getX(), event.getY())) {
+                    System.out.println("Shape is on");
                     if (event.isShiftDown()) {
+                        System.out.println("Shift is down");
                         handleSelectionWhenShiftIsDown(shape);
                     } else {
+                        System.out.println("Shift NOT down");
                         removeAllFromSelection();
                         addSelectedShapeToSelection(shape);
                     }
                     break;
-                } else {
-                    removeAllFromSelection();
                 }
             }
         }
@@ -40,23 +41,28 @@ public class SelectionHandler implements EventHandler<MouseEvent> {
 
     private void handleSelectionWhenShiftIsDown(IShape shape) {
         if (!listSelectedShape.contains(shape)) {
+            System.out.println("handleSelectionWhenShiftIsDown - addSelectedShapeToSelection");
             addSelectedShapeToSelection(shape);
         } else {
+            System.out.println("handleSelectionWhenShiftIsDown - removeSelectedShapeToSelection");
             removeSelectedShapeToSelection(shape);
         }
     }
 
     private void removeSelectedShapeToSelection(IShape shape) {
+        System.out.println("Remove selected shape to selection:" + shape);
         shape.setSelected(false);
         listSelectedShape.remove(shape);
     }
 
     private void addSelectedShapeToSelection(IShape shape) {
+        System.out.println("Add selected shape to selection:" + shape);
         shape.setSelected(true);
         listSelectedShape.add(shape);
     }
 
     private void removeAllFromSelection() {
+        System.out.println("Remove all from selection:");
         listSelectedShape.forEach(shape -> shape.setSelected(false));
         listSelectedShape.clear();
     }
