e6c7a3f81af37db4dafdec20aa1ed6f436ed7be0
Vitse Maxime <maxime.vitse@decathlon.com>
2023-01-29 19:36:28 +0100

    [minipaint] TP6 - Commands : Undo/Redo - TUs

--- minipaint/src/test/java/PaintTest.java
+++ minipaint/src/test/java/PaintTest.java
@@ -13,7 +13,6 @@ import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 
 
-import java.util.Iterator;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -21,10 +20,12 @@ import static org.junit.Assert.*;
 public class PaintTest extends ApplicationTest {
 
     PaintApplication app;
+    Stage stage;
 
     @Override
     public void start(Stage stage) {
         try {
+            this.stage = stage;
             app = new PaintApplication();
             app.start(stage);
         } catch (Exception e) {
@@ -171,7 +172,64 @@ public class PaintTest extends ApplicationTest {
 
         clickOn(".rectangle");
 
-        final List<IShape> shapes = app.getDrawingPane().getListSelectedShapes();
-        Assert.assertTrue(shapes.get(0) instanceof  ShapeGroupComposite);
+        final List<IShape> shapes = app.getDrawingPane().getShapes();
+        Assert.assertEquals(ShapeGroupComposite.class, shapes.get(0).getClass());
+    }
+
+    private Shape create_shape(String shapeType) {
+        //int x = 20 + get_nb_shapes() * 150;
+        int w = 50 + (int) (70 * Math.random());
+        int x = 20 + (int) ((stage.getWidth()-w) * Math.random());
+        int y = 50 + (int) ((stage.getHeight()-w) * Math.random());
+        clickOn("#"+shapeType);
+        moveTo(stage.getX()+x, stage.getY()+y)
+                .drag().dropBy(w,w);
+        return (Shape) app.getDrawingPane().getChildren().get(app.getDrawingPane().getChildren().size()-1);
+    }
+
+    @Test
+    public void should_group_ungroup(){
+        Shape s1 = create_shape("Rectangle");
+        Shape s2 = create_shape("Circle");
+        clickOn(s1);
+        press(KeyCode.SHIFT).moveTo(s2).clickOn();
+        release(KeyCode.SHIFT);
+        clickOn("#Group");
+
+        clickOn(s1);
+        press(KeyCode.SHIFT).moveTo(s2).clickOn();
+        clickOn("#Ungroup");
+
+        clickOn(".rectangle");
+        final List<IShape> shapes = app.getDrawingPane().getShapes();
+        Assert.assertNotEquals(ShapeGroupComposite.class, shapes.get(0).getClass());
+    }
+
+    @Test
+    public void should_undo(){
+        Shape s1 = create_shape("Rectangle");
+        Shape s2 = create_shape("Circle");
+        clickOn(s1);
+        press(KeyCode.SHIFT).moveTo(s2).clickOn();
+        release(KeyCode.SHIFT);
+        clickOn("#Group");
+        clickOn("#Undo");
+        clickOn(".rectangle");
+        final List<IShape> shapes = app.getDrawingPane().getShapes();
+        Assert.assertEquals(ShapeGroupComposite.class, shapes.get(0).getClass());
+    }
+
+    @Test
+    public void should_redo(){
+        Shape s1 = create_shape("Rectangle");
+        Shape s2 = create_shape("Circle");
+        clickOn(s1);
+        press(KeyCode.SHIFT).moveTo(s2).clickOn();
+        release(KeyCode.SHIFT);
+        clickOn("#Group");
+        clickOn("#Redo");
+        clickOn("#Rectangle");
+        final List<IShape> shapes = app.getDrawingPane().getShapes();
+        Assert.assertEquals(ShapeGroupComposite.class, shapes.get(0).getClass());
     }
 }
